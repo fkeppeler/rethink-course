8de753084a640c60dc648d84c6549b0eda81e9ec
fkeppeler <fkeppeler@gmail.com>
2022-02-27 15:35:15 -0600

    chapter2

--- load_packages.R
+++ load_packages.R
@@ -3,4 +3,4 @@
 # devtools::install_github("stan-dev/cmdstanr")
 # devtools::install_github("rmcelreath/rethinking",ref="Experimental")
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(dplyr, coda, mvtnorm, devtools,rethinking)
+pacman::p_load(dplyr, coda, mvtnorm, devtools, rethinking)
